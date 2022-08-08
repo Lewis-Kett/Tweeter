bf21f1d0d74e628d2a87dc3167346c0228d603e9
Lewis_Kett <lewis.a.kett@gmail.com>
2022-08-08 15:57:42 +0100

    adds twitter embedd and moves styles.css

--- components/Widgets/Widgets.tsx
+++ components/Widgets/Widgets.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { SearchIcon } from "@heroicons/react/outline";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 import { SearchInput } from "../SearchInput";
 
 export const Widgets = () => {
   return (
     <div className="mt-2 px-2">
       <SearchInput />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="elonmusk"
+        options={{ height: 1000 }}
+      />
     </div>
   );
 };
