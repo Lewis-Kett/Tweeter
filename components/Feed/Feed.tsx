45c7ec6d335499db198dc79d4ec9e2451a62a4a1
Lewis_Kett <lewis.a.kett@gmail.com>
2022-08-08 17:04:58 +0100

    adds tweet-box to feed composition

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { RefreshIcon } from "@heroicons/react/outline";
+import { TweetBox } from "../TweetBox";
 
 interface FeedProps {}
 
 export const Feed = ({}: FeedProps) => {
   return (
-    <div className="flex items-center justify-between">
-      <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-      <RefreshIcon
-        className="mr-5 mt-5 h-8 w-8 cursor-pointer 
-        text-twitter transition-all duration-500 ease-out 
-        hover:rotate-180 active:scale-125"
-        data-testid="refresh"
-      />
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <RefreshIcon
+          className="mr-5 mt-5 h-8 w-8 cursor-pointer 
+          text-twitter transition-all duration-500 ease-out 
+          hover:rotate-180 active:scale-125"
+          data-testid="refresh"
+        />
+      </div>
+      <div>
+        <TweetBox />
+      </div>
     </div>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { SideBar } from "./SideBar";
 export { SideBarRow } from "./SideBarRow";
 export { Feed } from "./Feed"
 export { SearchInput } from "./SearchInput"
+export { TweetBox } from "./TweetBox"
