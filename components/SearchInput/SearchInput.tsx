80497f96ba28601f60455f18ecaf828e04f48da3
Lewis_Kett <lewis.a.kett@gmail.com>
2022-08-31 10:11:59 +0100

    adds mb to search input

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -3,7 +3,7 @@ import { SearchIcon } from "@heroicons/react/outline";
 
 export const SearchInput = ({}) => {
   return (
-      <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+      <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3 mb-3">
         <SearchIcon className="h-5 w-5 text-gray-400" data-testid="searchIcon"/>
         <input
           type="text"
