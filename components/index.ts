415c37089d802b52c5d7e0d5c66892da85defcab
Lewis_Kett <lewis.a.kett@gmail.com>
2022-09-06 11:45:24 +0100

    updates comp exports

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,8 @@
 export { SideBar } from "./SideBar";
 export { SideBarRow } from "./SideBarRow";
-export { Feed } from "./Feed"
-export { SearchInput } from "./SearchInput"
-export { TweetBox } from "./TweetBox"
+export { Feed } from "./Feed";
+export { SearchInput } from "./SearchInput";
+export { TweetBox } from "./TweetBox";
+export { Comment } from "./Comment";
+export { Tweet } from "./Tweet";
+export { Widgets } from "./Widgets";
