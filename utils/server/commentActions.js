23d25b681343330cd2b1de100c3c37c16d0635d6
Lewis_Kett <lewis.a.kett@gmail.com>
2022-09-09 11:35:32 +0100

    adds sorting to comment endpoint

--- utils/server/commentActions.js
+++ utils/server/commentActions.js
@@ -6,10 +6,12 @@ export const createComment = async (newComment) =>
   await CommentModel.create(newComment);
 
 //get all comments
-export const getComments = async () => await CommentModel.find({}).lean().exec();
+export const getComments = async () =>
+  await CommentModel.find({}).sort({ createdAt: -1 }).lean().exec();
 
 //get all comments for tweet
-export const getCommentsForTweet = async (tweetId) => await CommentModel.find({tweetId}).lean().exec();
+export const getCommentsForTweet = async (tweetId) =>
+  await CommentModel.find({ tweetId }).sort({ createdAt: -1 }).lean().exec();
 
 //get one comment
 export const getComment = async (id) =>
@@ -20,4 +22,5 @@ export const updateComment = async (id, updatedComment) =>
   await CommentModel.findByIdAndUpdate(id, updatedComment, { new: true });
 
 //delete comment
-export const deleteComment = async (id) => await CommentModel.findByIdAndRemove(id);
+export const deleteComment = async (id) =>
+  await CommentModel.findByIdAndRemove(id);
